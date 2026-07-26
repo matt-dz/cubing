2cd3ab369be3bf4bcfd83f06b2ceaf88c3454622
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2026-07-26 14:13:36 -0400

    feat: add umami analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	{#if !dev}
+		<script
+			defer
+			src="https://umami.deguzman.cloud/script.js"
+			data-website-id="44bbc683-5cf9-4275-8939-516f820937aa"
+		></script>
+	{/if}
+</svelte:head>
 {@render children()}
