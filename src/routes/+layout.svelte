1f78acc275d2b003e15bb9c792cf44070ce3c3fe
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2026-07-26 15:00:40 -0400

    fix: place icons in proper directory and refence them in head section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,5 +15,10 @@
 			data-website-id="44bbc683-5cf9-4275-8939-516f820937aa"
 		></script>
 	{/if}
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 {@render children()}
